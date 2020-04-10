deac356dc36a8bf8d3a47080b90f13a0ab96967c
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-04-10 18:23:06 +0800

    update example

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-console.log('Hi typescript')
-console.log('Welcome, kbaluyot')
+/**
+ * Greet returns a good morning greeting message.
+ *
+ * @param   {string}  name
+ *
+ * @return  {string}
+ */
+function greet(name: string): string {
+  return `Good morning, ${name}!`
+}
+
+// print my greetings to you
+console.log(greet('Karl Anthony Baluyot'))
